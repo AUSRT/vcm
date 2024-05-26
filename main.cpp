6e75048bb4ff024946f84f3eebc7be9f53d00a0e
David Sutton <davidsutton@ausocean.org>
2024-05-26 15:16:48 +0930

    Add led toggle button

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
+#include "DigitalOut.h"
+#include "InterfaceDigitalIn.h"
+#include "InterruptIn.h"
+#include "PinNameAliases.h"
+#include "PinNames.h"
+#include "PinNamesTypes.h"
+#include "ThisThread.h"
 #include "mbed.h"
+#include <cstdio>
+
+// Setup gpio.
+InterruptIn btn(D2, PullDown);
+DigitalOut led(LED1);
+DigitalOut flash(LED2);
+
+// flip() flips the output of the LED from on to off
+void flip() {
+    led = !led;
+    return;
+}
 
 // main() runs in its own thread in the OS
 int main()
-{
+{    
+    btn.rise(&flip);
     while (true) {
-
+        flash = !flash;
+        ThisThread::sleep_for(500ms);
     }
 }
 
